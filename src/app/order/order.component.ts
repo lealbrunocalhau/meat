dd39f1785f8df59a9460d6c39f372c69c60a56ad
lealcalhau <calhau.bruno88@gmail.com>
2018-08-23 16:13:43 -0300

    adicionando evento blur nos forms

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormBuilder,FormControl, Validators, AbstractControl} from '@angular/forms'
+import {Router} from '@angular/router'
 import { RadioOption } from '../shared/radio/radio-option.model'
 import { OrderService } from './order.service'
 import { CartItem } from '../restaurant-detail/shopping-cart/cart-item.model'
 import {Order, OrderItem} from './order.model'
-import {Router} from '@angular/router'
-import {FormGroup, FormBuilder, Validators, AbstractControl} from '@angular/forms'
 
 import 'rxjs/add/operator/do'
 
@@ -36,15 +36,17 @@ export class OrderComponent implements OnInit {
               private formBuilder: FormBuilder) { }
 
   ngOnInit() {
-    this.orderForm = this.formBuilder.group({
-      name: this.formBuilder.control('',[Validators.required, Validators.minLength(5)]),
+    this.orderForm = new FormGroup({
+      name: new FormControl('',{
+        validators: [Validators.required, Validators.minLength(5)]
+      }),
       email: this.formBuilder.control('',[Validators.required, Validators.pattern(this.emailPattern)]),
       emailConfirmation: this.formBuilder.control('',[Validators.required, Validators.pattern(this.emailPattern)]),
       address: this.formBuilder.control('',[Validators.required, Validators.minLength(5)]),
       number: this.formBuilder.control('',[Validators.required, Validators.pattern(this.numberPattern)]),
       optionalAddress: this.formBuilder.control(''),
       paymentOption: this.formBuilder.control('',[Validators.required]),
-    }, {validator: OrderComponent.equalsTo})
+    }, {validators: [OrderComponent.equalsTo], updateOn: 'blur' })
   }
 
   static equalsTo(group: AbstractControl): {[key:string]: boolean}{
